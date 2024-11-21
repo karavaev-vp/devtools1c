b49b1de954a1a799cfc9ff187a90b9dce6056661
Владимир Караваев <karavaev-vp@mail.ru>
2024-11-21 22:54:34 +0300

    Добавлены файлы очистки кэша 1С

---
+++ ClearUserCache1C.bat
@@ -0,0 +1,5 @@
+rd /q /s "%userprofile%\AppData\Local\1C\1Cv82\"
+rd /q /s "%userprofile%\AppData\Roaming\1C\1Cv82\"
+rd /q /s "%userprofile%\AppData\Local\1C\1Cv8\"
+rd /q /s "%userprofile%\AppData\Roaming\1C\1Cv8\"
+pause
